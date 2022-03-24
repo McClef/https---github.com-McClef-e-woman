3cfa5f98b6cd76ce076fc7285d7c78aae2abb0ca
McClef <clefmaj@gmail.com>
2022-03-24 09:10:19 +0100

    Modal effect

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Copyright Year
+
 const yearSpan = document.querySelector('#currentYear')
+
 const currentYear = new Date();
 yearSpan.innerText = currentYear.getFullYear();
+
+// Modal Script
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
